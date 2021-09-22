665d050e5ec0f20f0f28fa7f391144b4463d0f6a
Marcin Januszewski <89555435+ichmura@users.noreply.github.com>
2021-09-22 11:28:30 +0200

    Delete include directory

--- include/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "serwer2079416.home.pl";
-$username = "33844940_burnoo";
-$password = "d2kxwvpk3j_1-";
-$database = "33844940_burnoo";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
